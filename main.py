8762c64f5e8532c7ac746385ab7a4cb196ef01a3
stasonucin <discordantstas@icloud.com>
2022-06-03 16:53:13 +0300

    add test abs

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-import math
+assert abs(-42) == 42 , "Ошибка значения"
 
-x= 11
-b = str(abs(12 * math.sin(x)))
+print("Let's count together: {}, then goes {}, and then {}".format("one", "two", "three"))
 
-str(math.ceil(math.pow(math.pi, math.e)*10000))
+str1 = "one"
+str2 = "two"
+str3 = "three"
+print(f"Let's count together: {str1}, then goes {str2}, and then {str3}")
+
+actual_result = "abrakadabra"
+print(f"Wrong text, got {actual_result}, something wrong")
+
+
+def test_input_text(expected_result, actual_result):
+    assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}"
+
+
+s = 'My Name is Julia'
+if 'Name' in s:
+        print('Substring found')
+
+
+index = s.find('Name')
+if index != 1:
+        print(f'Substring found at index {index}')
+
+
+def test_substring(full_string, substring):
+    assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
+
