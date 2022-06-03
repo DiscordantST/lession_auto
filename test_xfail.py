49bb4717669991dddae5eac0d42c5bef4e842388
stasonucin <discordantstas@icloud.com>
2022-06-03 22:39:28 +0300

    add test abs

---
+++ test_xfail.py
@@ -0,0 +1,19 @@
+import condition as condition
+import pytest
+import condition
+import reason as reason
+
+
+@pytest.mark.xfail(strict=True)
+def test_succeed():
+    assert True
+
+
+@pytest.mark.xfail
+def test_not_succeed():
+    assert False
+
+
+@pytest.mark.skip
+def test_skipped():
+    assert False
